2f2f9538a48a17349b4ce97cef844e04493a72b6
Ahmadullah mirza <mirzaahmad9028.am@gmail.com>
2025-02-09 00:00:43 +0530

    Feat: implement caching for Dua fetching and add fetchDuaById and fetchDuaOfTheDay functions

--- services/ApiService.ts
+++ services/ApiService.ts
@@ -2,17 +2,52 @@ import categories from "@/data/categories.json";
 import duas from "@/data/duas.json";
 import { waitFor } from "@/utils/wait";
 import { uuid } from "@/utils/uuid";
+import { useStore } from "@/store/store";
+
+const DUA_API_URL =
+  "https://secure.quranexplorer.com/DuaAppServices/Service1.svc/GetDuaDetailByDuaID/";
 
 export async function fetchDuas() {
-  await waitFor(1000);
-  return duas.filter((d) => d.DuaID) as IDuaItem[];
+  await waitFor(100);
+  return duas.filter((d) => Boolean(d.DuaID)) as IDuaItem[];
 }
 
 export async function fetchDuasCategories() {
-  await waitFor(1000);
+  await waitFor(100);
   const mappedCategories: ICategory[] = categories.map((c, index) => ({
     ...c,
     id: uuid(),
   }));
   return mappedCategories;
+}
+
+export async function fetchDuaById(duaId: number): Promise<IDua> {
+  const { getCachedDua, cacheDua } = useStore.getState();
+  const cachedDua = getCachedDua(duaId);
+
+  if (cachedDua) {
+    return cachedDua;
+  }
+
+  const response = await fetch(DUA_API_URL + duaId);
+  const data = await response.json();
+  const dua = { ...data, DuaID: duaId };
+
+  cacheDua(dua);
+  return dua;
+}
+
+export async function fetchDuaOfTheDay() {
+  const { getDuaOfTheDay, setDuaOfTheDay } = useStore.getState();
+  const { dua, expired } = getDuaOfTheDay();
+
+  if (dua && !expired) {
+    return dua;
+  }
+
+  const validDuas = duas.filter((d) => Boolean(d.DuaID)) as IDuaItem[];
+  const randomIndex = Math.floor(Math.random() * validDuas.length);
+  const newDua = (await fetchDuaById(validDuas[randomIndex].DuaID)) as IDua;
+  setDuaOfTheDay(newDua);
+  return newDua;
 }
